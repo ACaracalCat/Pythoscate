5494b51e03435ecb72aa90cc6504be6f5e2cfe4b
ARagdollCat <115324965+ACaracalCat@users.noreply.github.com>
2024-06-23 18:54:41 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from tkinter import filedialog
+import base64
+import codecs
+import time
+
+print("Ragdoll Obfuscator Launched. Please Select a file.")
+
+def file_select():
+    root = tk.Tk()
+    root.withdraw()
+    path = filedialog.askopenfilename(filetypes=[("Python files", "*.py")])
+    return path
+
+path = file_select()
+
+if path:
+    for i in range(10):
+        with open(path, 'r') as file:
+            script_content = file.read()
+
+        base64_encoded = base64.b64encode(script_content.encode()).decode()
+        rot13_encoded = codecs.encode(base64_encoded, 'rot_13')
+        ascii85_encoded = base64.a85encode(rot13_encoded.encode()).decode()
+
+        encoded_exec = (
+            "import base64\n"
+            "import codecs\n"
+            f"exec(base64.b64decode(codecs.decode(base64.a85decode({repr(ascii85_encoded)}).decode(), 'rot_13')).decode())"
+        )
+
+        with open(path, 'w') as file:
+            file.write(encoded_exec)
+
+        print(f"Obfuscated {i+1} time(s)")
+    
+    print(f"Obfuscated {path}!")
+else:
+    print("No file selected...")
+
+time.sleep(2.5)
+exit()
